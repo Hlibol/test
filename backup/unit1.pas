97e0b7ca70e064c48cc5f7afccaafbb981e865c6
Hlibol <olijnyk@kpnl145.kyiv.ua>
2024-11-26 09:43:47 +0200

    upd

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -14,15 +14,16 @@ type
             amount:longint;
             answear:array[0..9] of string;
             correct:array[0..9] of boolean;
+            usanswear:array[0..9] of boolean;
   end;
 
   { TForm1 }
 
   TForm1 = class(TForm)
     btn_prev: TButton;
-    btn_next: TButton;
-    Btn_result: TButton;
     btn_start: TButton;
+    btn_next: TButton;
+    btn_result: TButton;
     CheckGroup1: TCheckGroup;
     Edit1: TEdit;
     Image1: TImage;
@@ -32,6 +33,7 @@ type
     RadioGroup1: TRadioGroup;
     procedure btn_prevClick(Sender: TObject);
     procedure btn_startClick(Sender: TObject);
+    procedure btn_nextClick(Sender: TObject);
     procedure CheckGroup1Click(Sender: TObject);
   private
 
@@ -56,8 +58,34 @@ procedure fillcheck(checkgroup1:TCheckGroup);
 begin
   CheckGroup1.items.clear;
   for j:=0 to mass[i].amount-1 do
-  begin
     CheckGroup1.items.add(mass[i].answear[j]);
+end;
+
+
+
+procedure chreadusanswear(checkgroup1:TCheckGroup);
+begin
+  for j:=0 to mass[i].amount-1 do
+  begin
+     mass[i].usanswear[j]:=checkgroup1.checked[j]
+  end;
+end;
+
+procedure rareadusanswear(radiogroup1:TRadioGroup);
+begin
+  for j:=0 to mass[i].amount-1 do
+  begin
+     mass[i].usanswear[j]:=false;
+  end;
+  mass[i].usanswear[radiogroup1.ItemIndex]:=true;
+end;
+
+procedure fillradio(radiogroup1:TRadioGroup);
+begin
+  radioGroup1.items.clear;
+  for j:=0 to mass[i].amount-1 do
+  begin
+    radioGroup1.items.add(mass[i].answear[j]);
   end;
 end;
 
@@ -68,6 +96,30 @@ end;
 
 procedure TForm1.btn_prevClick(Sender: TObject);
 begin
+    if mass[i].typ='radio' then
+      rareadusanswear(radiogroup1);
+   if mass[i].typ='check' then
+      chreadusanswear(checkgroup1);
+   btn_next.visible:=true;
+   Btn_result.visible:=false;
+   if i=2 then
+   begin
+      btn_prev.visible:=false;
+   end;
+   dec(i);
+   label1.caption:=mass[i].question;
+   if mass[i].typ='check' then
+   begin
+       checkgroup1.visible:=true;
+       radiogroup1.visible:=false;
+       fillcheck(checkgroup1);
+   end;
+   if mass[i].typ='radio' then
+   begin
+       radiogroup1.visible:=true;
+       checkgroup1.visible:=false;
+       fillradio(radiogroup1);
+   end;
 
 end;
 
@@ -99,7 +151,7 @@ begin
      end;
      inc(i);
    end;
-   N:=i;
+   N:=i-1;
    edit1.Visible:=false;
    btn_start.Visible:=false;
    label2.Visible:=false;
@@ -112,7 +164,39 @@ begin
        fillcheck(checkgroup1);
    end;
    if mass[i].typ='radio' then
+   begin
        radiogroup1.visible:=true;
+       fillradio(radiogroup1);
+   end;
+end;
+
+procedure TForm1.btn_nextClick(Sender: TObject);
+begin
+   if mass[i].typ='radio' then
+      rareadusanswear(radiogroup1);
+   if mass[i].typ='check' then
+      chreadusanswear(checkgroup1);
+   btn_prev.visible:=true;
+   if i=n-1 then
+   begin
+      btn_next.visible:=false;
+      Btn_result.visible:=true;
+   end;
+   inc(i);
+   label1.caption:=mass[i].question;
+   if mass[i].typ='check' then
+   begin
+       checkgroup1.visible:=true;
+       radiogroup1.visible:=false;
+       fillcheck(checkgroup1);
+   end;
+   if mass[i].typ='radio' then
+   begin
+       radiogroup1.visible:=true;
+       checkgroup1.visible:=false;
+       fillradio(radiogroup1);
+   end;
+
 end;
 
 end.
